977630aea8ec3954212007894d03c0ecc56feeb2
Pedro Sanchez <pedrosanchez201212@gmail.com>
2021-06-24 21:48:27 -0400

    feat: Hero page responsive

--- index.js
+++ index.js
@@ -1,50 +1,62 @@
 // Photo movement animation
-let Photo = document.querySelector(".Photo");
-let PhotoData = document.querySelector(".PhotoData");
-let cursorPhoto = {
-  x: 0,
-  y: 0,
-};
-let cursorPhotoData = {
-  x: 0,
-  y: 0,
-};
+let isDesktop = window.matchMedia("(min-width: 77rem)");
 
-document.addEventListener("mousemove", (event) => {
-  cursorPhoto.x = -event.clientX * 0.025;
-  cursorPhoto.y = -event.clientY * 0.025;
-  cursorPhotoData.x = -event.clientX * 0.03 * 1.4;
-  cursorPhotoData.y = -event.clientY * 0.03 * 1.5;
+function MouseMoveAnimation(isDesktop) {
+  if (!isDesktop.matches) return;
 
-  Photo.style.transform = `translate(${cursorPhoto.x}px, ${cursorPhoto.y}px)`;
-  PhotoData.style.transform = `translate(${cursorPhotoData.x}px, ${cursorPhotoData.y}px)`;
-});
+  let Photo = document.querySelector(".Photo");
+  let PhotoData = document.querySelector(".PhotoData");
+  let cursorPhoto = {
+    x: 0,
+    y: 0,
+  };
+  let cursorPhotoData = {
+    x: 0,
+    y: 0,
+  };
 
-let Title = CSSRulePlugin.getRule(".title::after");
-let MainParagraph = CSSRulePlugin.getRule(".MainParagraph::after");
-let HeroTl = gsap.timeline({ defaults: { duration: 2 } });
+  document.addEventListener("mousemove", (event) => {
+    cursorPhoto.x = -event.clientX * 0.025;
+    cursorPhoto.y = -event.clientY * 0.025;
+    cursorPhotoData.x = -event.clientX * 0.03 * 1.4;
+    cursorPhotoData.y = -event.clientY * 0.03 * 1.5;
 
-HeroTl.from(".MainContent", {
-  y: -50,
-  opacity: 0,
-  ease: "Power1.easeOut",
-})
-  .to(
-    [Title, MainParagraph],
-    {
-      cssRule: {
-        scaleY: 0,
+    Photo.style.transform = `translate(${cursorPhoto.x}px, ${cursorPhoto.y}px)`;
+    PhotoData.style.transform = `translate(${cursorPhotoData.x}px, ${cursorPhotoData.y}px)`;
+  });
+}
+
+function HeroTimeline() {
+  let Title = CSSRulePlugin.getRule(".title::after");
+  let MainParagraph = CSSRulePlugin.getRule(".MainParagraph::after");
+  let HeroTl = gsap.timeline({ defaults: { duration: 2 } });
+
+  HeroTl.from(".MainContent", {
+    y: -50,
+    opacity: 0,
+    ease: "Power1.easeOut",
+  })
+    .to(
+      [Title, MainParagraph],
+      {
+        cssRule: {
+          scaleY: 0,
+        },
+        duration: 1.8,
+        ease: "Power1.easeOut",
       },
-      duration: 1.8,
-      ease: "Power1.easeOut",
-    },
-    "<"
-  )
-  .from(
-    ".Break",
-    {
-      opacity: 0,
-      ease: "Power1.easeOut",
-    },
-    "<"
-  );
+      "<"
+    )
+    .from(
+      ".Break",
+      {
+        opacity: 0,
+        ease: "Power1.easeOut",
+      },
+      "<"
+    );
+}
+
+HeroTimeline();
+MouseMoveAnimation(isDesktop);
+isDesktop.addListener(MouseMoveAnimation);
